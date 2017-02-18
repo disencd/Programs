1beb918021fa49acfa11d8c4ae07d3c69f6457a8
DisenCD <disencd@gmail.com>
2017-02-18 10:44:54 -0800

    Create 100_python_program_4_convert_tuple_list.py

---
+++ 100_python_program_4_convert_tuple_list.py
@@ -0,0 +1,26 @@
+'''
+Program : 100_python_program_4_convert_tuple_list.py
+Question:
+Write a program which accepts a sequence of comma-separated
+numbers from console and generate a list and a tuple which
+contains every number.
+Suppose the following input is supplied to the program:
+34,67,55,33,12,98
+Then, the output should be:
+['34', '67', '55', '33', '12', '98']
+('34', '67', '55', '33', '12', '98')
+
+Hints:
+In case of input data being supplied to the question, it should be assumed to be a console input.
+tuple() method can convert list to tuple
+
+'''
+
+str = raw_input("Accepts a sequence of comma-separated numbers:::")
+
+l = str.split(",")
+
+tup = tuple(l)
+
+print(l)
+print(tup)
