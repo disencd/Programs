b9acc7acf9091e810e1a5608a031a15f7484658a
DisenCD <disencd@gmail.com>
2017-02-26 09:58:56 -0800

    Create 100_python_program_24_reverse_string.py

---
+++ 100_python_program_24_reverse_string.py
@@ -0,0 +1,13 @@
+'''
+Programs:100_python_program_24_reverse_string.py
+
+Question:
+    Write a method which can reverse the string
+
+
+Solution:
+'''
+def reverse(inp):
+    return inp[::-1]
+
+print reverse("Disencd")
