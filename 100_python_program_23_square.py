84a17c6d5058eedad0c01dfc5cad0f0e6b0dfd99
DisenCD <disencd@gmail.com>
2017-02-26 09:59:34 -0800

    Create 100_python_program_23_square.py

---
+++ 100_python_program_23_square.py
@@ -0,0 +1,16 @@
+'''
+Programs:100_python_program_23_square.py
+
+Question:
+    Write a method which can calculate square value of number
+
+Hints:
+    Using the ** operator
+
+Solution:
+'''
+def square(num):
+    return num ** 2
+
+print square(2)
+print square(3)
